d0c5f069a075cf853ce8a8166c81f212f434184f
PUTRI Shinta <23050681@UYM.ac.id>
2026-07-14 00:05:31 +0700

    Configure MySQL connection for Railway

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "shinta_ilkom",
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 module.exports = db.promise();
